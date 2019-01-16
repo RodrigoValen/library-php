b93b8b146455cd2d6c03166a52d3fb3d244d59d8
Nabetse8 <esteban.tobarz@mayor,cl>
2019-01-16 11:39:01 -0300

    Delete y todos funcionando

--- apps/index/modules/blog/action/delcerveza/action-default.php
+++ apps/index/modules/blog/action/delcerveza/action-default.php
@@ -9,7 +9,7 @@ if (count($_GET) > 0)
 
     $user = $_SESSION['nombre_usuario'];
     $pass = $_SESSION['password'];
-    $ch = curl_init('http://api.catalogos.local/productos.php'.$_GET["id"]);
+    $ch = curl_init('http://api.catalogos.local/productos.php?id='.$_GET["id"]);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
@@ -19,7 +19,7 @@ if (count($_GET) > 0)
 
     curl_close($ch);
 
-    Core::redir("./index.php?view=libros");
+    Core::redir("./index.php?view=producto");
 
 }
 
